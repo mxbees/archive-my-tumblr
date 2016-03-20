2fb1f8a055f98c7692fe4641a78c3ca4e159a116
b. binaohan <b@binaohan.org>
2016-03-20 14:44:12 -0400

    i think this might be working...

--- archive-posts.rb
+++ archive-posts.rb
@@ -10,154 +10,76 @@ require 'reverse_markdown'
 require 'csv'
 
 class Archive
-  @@client = Tumblr::Client.new({
-    :consumer_key => '6t1TegHgZM01spIJnsLjZtIHM6c7qR5BzHHGuAKfMz4ecIzenK',
-    :consumer_secret => 'ELhQmF3M6ZNsCYp0eKw49iNfOcVVXvcp7Vu5GV71hvDJVQiJNJ',
-    :oauth_token => 'wfXWoOFX66FQDsY3SMmdrsX2pZEQW3cPLHlNNU3Mb48sHrsV65',
-    :oauth_token_secret => 'YgvcRM1y1UwI8oZwCyhS0NRNbURaIdf5UUaOpS7gZcwPAaPNQj'
-  })
-  all_data = @@client.posts("mxb.ca", :limit => 1)
-  @@total_posts = all_data.fetch("total_posts")
+  def api_call
+    x = 0
+    config = YAML.load_file('.config.yml')
+    client = Tumblr::Client.new({
+      :consumer_key => config.fetch('consumer key'),
+      :consumer_secret => config.fetch('consumer secret'),
+      :oauth_token => config.fetch('oauth token'),
+      :oauth_token_secret => config.fetch('oauth secret')
+    })
+    all_data = client.posts("mxb.ca", :limit => 1)
+    @@total_posts = 1#all_data.fetch("total_posts")
+    post_data = client.posts("mxb.ca", :filter => 'raw', :offset => x, :reblog_info => true, :limit => 1)
+    post_js = post_data.fetch('posts')
+    @@post_hash = post_js[0]
+  end
   
-  def text
-  x = 0  
+  def loop
     @@total_posts.times do
-      post_data = @@client.posts("mxb.ca", :filter => 'raw', :offset => x, :reblog_info => true, :type => "text", :limit => 1)
-      post_js = post_data.fetch('posts')
-      post_hash = post_js[0]
-      test  = post_hash.has_key?('reblogged_from_id')
-      if test == false
-        metadata = post_hash.select{|k,v| k == "id" || k == "slug" || k == "date" || k == "tags" || k == "title" || k == "type"}
-        body = post_hash.fetch("body")
-        post_date = post_hash.fetch("date")
-        iso_date = Date.parse(post_date)
-        title = post_hash.fetch("slug")
-        file_name = "#{iso_date}_#{title}.md"
-        IO.write("original/text/#{file_name}", metadata.to_yaml)
-        File.open("original/text/#{file_name}", mode="a") { |file| file.write("---\n") }
-        File.open("original/text/#{file_name}", mode="a") { |file| file.write(body)}
-      else
-        metadata = post_hash.select{|k,v| k == "id" || k == "slug" || k == "date" || k == "tags" || k == "title" || k == "type"}
-        body = post_hash.fetch("body")
-        post_date = post_hash.fetch("date")
-        iso_date = Date.parse(post_date)
-        title = post_hash.fetch("slug")
-        file_name = "#{iso_date}_#{title}.md"
-        IO.write("reblog/text/#{file_name}", metadata.to_yaml)
-        File.open("reblog/text/#{file_name}", mode="a") { |file| file.write("---\n") }
-        File.open("reblog/text/#{file_name}", mode="a") { |file| file.write(body)}
-      end
-    x += 1
-    #sleep 1
+    
     end
   end
   
+  def sort_reblogs
+    test  = @@post_hash.has_key?('reblogged_from_id')
+    type = @@post_hash.fetch('type')
+    if test == false
+      op = original
+      metadata = archive.'#{type}'
+      archive.make_record(op, type, metadata)
+    else
+      op = reblog
+      metadata = archive.'#{type}'
+      archive.make_record(op, type, metadata)
+    end
+  end
+  
+  def make_record(op, type, metadata)
+    post_date = @@post_hash.fetch("date")
+    iso_date = Date.parse(post_date)
+    title = @@post_hash.fetch("slug")
+    file_name = "#{iso_date}_#{title}.md"
+    IO.write("#{op}/#{type}/#{file_name}", metadata.to_yaml)
+    File.open("#{op}/#{type}/#{file_name}", mode="a") { |file| file.write("---\n") }
+    File.open("#{op}/#{type}/#{file_name}", mode="a") { |file| file.write(body)}
+  end
+  
+  def text
+    metadata = @@post_hash.select{|k,v| k == "id" || k == "slug" || k == "date" || k == "tags" || k == "title" || k == "type"}
+    body = @@post_hash.fetch("body")
+  end
+  
   def link
-  x = 0
-    @@total_posts.times do
-      post_data = @@client.posts("mxb.ca", :filter => 'raw', :offset => x, :reblog_info => true, :type => "link", :limit => 1)
-      post_js = post_data.fetch('posts')
-      post_hash = post_js[0]
-      test  = post_hash.has_key?('reblogged_from_id')
-      if test == false
-        metadata = post_hash.select{|k,v| k == "id" || k == "slug" || k == "date" || k == "tags" || k == "title" || k == "url" || k == "type"}
-        body = post_hash.fetch("description")
-        #md_body = ReverseMarkdown.convert html_body
-        post_date = post_hash.fetch("date")
-        iso_date = Date.parse(post_date)
-        title = post_hash.fetch("slug")
-        file_name = "#{iso_date}_#{title}.md"
-        IO.write("original/link/#{file_name}", metadata.to_yaml)
-        File.open("original/link/#{file_name}", mode="a") { |file| file.write("---\n") }
-        File.open("original/link/#{file_name}", mode="a") { |file| file.write(body)}
-      else
-        metadata = post_hash.select{|k,v| k == "id" || k == "slug" || k == "date" || k == "tags" || k == "title" || k == "url" || k == "type"}
-        body = post_hash.fetch("description")
-        #md_body = ReverseMarkdown.convert html_body
-        post_date = post_hash.fetch("date")
-        iso_date = Date.parse(post_date)
-        title = post_hash.fetch("slug")
-        file_name = "#{iso_date}_#{title}.md"
-        IO.write("reblog/link/#{file_name}", metadata.to_yaml)
-        File.open("reblog/link/#{file_name}", mode="a") { |file| file.write("---\n") }
-        File.open("reblog/link/#{file_name}", mode="a") { |file| file.write(body)}
-      end
-      x += 1
+    metadata = @@post_hash.select{|k,v| k == "id" || k == "slug" || k == "date" || k == "tags" || k == "title" || k == "url" || k == "type"}
+    body = @@post_hash.fetch("description")
     end
   end
   
   def photo
-  x = 0
-    @@total_posts.times do
-      post_data = @@client.posts("mxb.ca", :filter => 'raw', :offset => x, :reblog_info => true, :type => "photo", :limit => 1)
-      post_js = post_data.fetch('posts')
-      post_hash = post_js[0]
-      test  = post_hash.has_key?('reblogged_from_id')
-      if test == false
-        metadata = post_hash.select{|k,v| k == "id" || k == "slug" || k == "date" || k == "tags" || k == "title" || k == "type"}
-        body = post_hash.fetch("caption")
-        photos = post_hash.fetch("photos")
-        photo_hash = photos[0]
-        original = photo_hash.fetch("original_size")
-		    original_url = original.fetch("url")
-        post_date = post_hash.fetch("date")
-        iso_date = Date.parse(post_date)
-        title = post_hash.fetch("slug")
-        file_name = "#{iso_date}_#{title}.md"
-        IO.write("original/photo/#{file_name}", metadata.to_yaml)
-        File.open("original/photo/#{file_name}", mode="a") { |file| file.write("original size: #{original_url}\n---\n") }
-        File.open("original/photo/#{file_name}", mode="a") { |file| file.write(body)}
-      else
-        metadata = post_hash.select{|k,v| k == "id" || k == "slug" || k == "date" || k == "tags" || k == "title" || k == "type"}
-        body = post_hash.fetch("caption")
-        photos = post_hash.fetch("photos")
-        photo_hash = photos[0]
-        original = photo_hash.fetch("original_size")
-		    original_url = original.fetch("url")
-        post_date = post_hash.fetch("date")
-        iso_date = Date.parse(post_date)
-        title = post_hash.fetch("slug")
-        file_name = "#{iso_date}_#{title}.md"
-        IO.write("reblog/photo/#{file_name}", metadata.to_yaml)
-        File.open("original/photo/#{file_name}", mode="a") { |file| file.write("original size: #{original_url}\n---\n") }
-        File.open("reblog/photo/#{file_name}", mode="a") { |file| file.write(body)}
-      end
-      x += 1
-    end
+    metadata = @@post_hash.select{|k,v| k == "id" || k == "slug" || k == "date" || k == "tags" || k == "title" || k == "type"}
+    body = @@post_hash.fetch("caption")
+    photos = @@post_hash.fetch("photos")
+    photo_hash = photos[0]
+    original = photo_hash.fetch("original_size")
+		original_url = original.fetch("url")
   end
   
   def answer
-  x = 0
-    @@total_posts.times do
-      post_data = @@client.posts("mxb.ca", :filter => 'raw', :offset => x, :reblog_info => true, :type => "answer", :limit => 1)
-      post_js = post_data.fetch('posts')
-      post_hash = post_js[0]
-      test  = post_hash.has_key?('reblogged_from_id')
-      if test == false
-        metadata = post_hash.select{|k,v| k == "id" || k == "slug" || k == "date" || k == "tags" || k == "title" || k == "type"}
-        question = post_hash.fetch("question")
-        answer = post_hash.fetch("answer")
-        post_date = post_hash.fetch("date")
-        iso_date = Date.parse(post_date)
-        title = post_hash.fetch("slug")
-        file_name = "#{iso_date}_#{title}.md"
-        IO.write("original/answer/#{file_name}", metadata.to_yaml)
-        File.open("original/answer/#{file_name}", mode="a") { |file| file.write("---\n") }
-        File.open("original/answer/#{file_name}", mode="a") { |file| file.write("question: #{question}\n\nanswer: #{answer}")}
-      else
-        metadata = post_hash.select{|k,v| k == "id" || k == "slug" || k == "date" || k == "tags" || k == "title" || k == "type"}
-        question = post_hash.fetch("question")
-        answer = post_hash.fetch("answer")
-        post_date = post_hash.fetch("date")
-        iso_date = Date.parse(post_date)
-        title = post_hash.fetch("slug")
-        file_name = "#{iso_date}_#{title}.md"
-        IO.write("reblog/answer/#{file_name}", metadata.to_yaml)
-        File.open("reblog/answer/#{file_name}", mode="a") { |file| file.write("---\n") }
-        File.open("reblog/answer/#{file_name}", mode="a") { |file| file.write("question: #{question}\n\nanswer: #{answer}")}
-        end
-      x += 1
-    end
+    metadata = @@post_hash.select{|k,v| k == "id" || k == "slug" || k == "date" || k == "tags" || k == "title" || k == "type"}
+    question = @@post_hash.fetch("question")
+    answer = @@post_hash.fetch("answer")
   end
   
   def post_ids
@@ -165,9 +87,9 @@ class Archive
     @@total_posts.times do
       post_data = client.posts("mxb.ca", :filter => 'raw', :offset => x, :type => 'text', :limit => 1)
       post_js = post_data.fetch('posts')
-      post_hash = post_js[0]
-      id = post_hash.fetch('id')
-      post_date = post_hash.fetch("date")
+      @@post_hash = post_js[0]
+      id = @@post_hash.fetch('id')
+      post_date = @@post_hash.fetch("date")
       iso_date = Date.parse(post_date)
       CSV.open("all-post-ids.csv", mode="a") do |csv|
         csv << [id, iso_date]
